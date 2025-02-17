36c205ddec1d5f07dc618e242b17c28dd536f679
Jonathan Dunlap <jdunlap@outlook.com>
2025-02-16 19:30:18 -0500

    fix up React to work again

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { hydrateRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { GameStateProvider } from "./hooks/useGameState";
+import "./index.css";
 
-hydrateRoot(
-  document.getElementById("root")!,
-  <GameStateProvider>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </GameStateProvider>
+  </StrictMode>
 );
